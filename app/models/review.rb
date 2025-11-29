b8b9c1ee60db492ea164fe12383c03432cdbfca3
Pato Arenas <patoska@gmail.com>
2025-11-29 12:30:28 -0300

    Added models validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  validates :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :body, length: { maximum: 1000 }
 end
